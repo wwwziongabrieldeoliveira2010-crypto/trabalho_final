5b5a806f8b1e469d89e1399d130ce8bdaf5c9078
Davi MB <bissondavi@gmail.com>
2026-05-21 14:38:49 -0300

    Update def_mostrar_materias.py

---
+++ def_mostrar_materias.py
@@ -0,0 +1,23 @@
+def mostrar_materias():
+
+    db = conectar()
+    cursor = db.cursor()
+
+    cursor.execute("""
+    SELECT id_materia, nome_materia
+    FROM materias
+    """)
+
+    materias = cursor.fetchall()
+
+    print("\n===== MATÉRIAS =====")
+
+    for materia in materias:
+
+        print(
+            f"ID: {materia[0]} | "
+            f"Nome: {materia[1]}"
+        )
+
+    cursor.close()
+    db.close()
