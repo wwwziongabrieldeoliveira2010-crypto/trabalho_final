55ceef40985e0a5bd8afe2d031eda6b0fb470d88
Zion Gabriel <www.ziongabrieldeoliveira2010@gmail.com>
2026-05-27 11:32:39 -0300

    sim

--- arquivo_utilitario.py
+++ arquivo_utilitario.py
@@ -12,6 +12,140 @@ cursor = db.cursor()
 
 cursor.execute("CREATE DATABASE IF NOT EXISTS boletim")
 
+cursor.execute("USE boletim")
+
+ 
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS usuarios (
+ 
+    id_usuario INT AUTO_INCREMENT PRIMARY KEY,
+ 
+    login VARCHAR(50) UNIQUE NOT NULL,
+ 
+    senha VARCHAR(255) NOT NULL,
+ 
+    cargo VARCHAR(20) NOT NULL
+)
+""")
+ 
+ 
+ 
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS alunos (
+ 
+    id_aluno INT AUTO_INCREMENT PRIMARY KEY,
+ 
+    nome VARCHAR(100) NOT NULL,
+ 
+    idade INT NOT NULL,
+ 
+    cpf CHAR(11) UNIQUE NOT NULL
+)
+""")
+ 
+ 
+ 
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS materias (
+ 
+    id_materia INT AUTO_INCREMENT PRIMARY KEY,
+ 
+    nome_materia VARCHAR(100) UNIQUE NOT NULL
+)
+""")
+ 
+ 
+ 
+materias = [
+    ('Matemática',),
+    ('Português',),
+    ('História',),
+    ('Geografia',),
+    ('Ciências',),
+    ('Inglês',)
+]
+ 
+cursor.executemany("""
+INSERT IGNORE INTO materias (nome_materia)
+VALUES (%s)
+""", materias)
+ 
+ 
+ 
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS notas (
+ 
+    id_nota INT AUTO_INCREMENT PRIMARY KEY,
+ 
+    nota FLOAT NOT NULL,
+ 
+    bimestre INT NOT NULL,
+ 
+    fk_id_aluno INT NOT NULL,
+ 
+    fk_id_materia INT NOT NULL,
+ 
+    FOREIGN KEY (fk_id_aluno)
+    REFERENCES alunos(id_aluno)
+    ON DELETE CASCADE,
+ 
+    FOREIGN KEY (fk_id_materia)
+    REFERENCES materias(id_materia)
+    ON DELETE CASCADE
+)
+""")
+ 
+ 
+ 
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS logs (
+ 
+    id_log INT AUTO_INCREMENT PRIMARY KEY,
+ 
+    usuario VARCHAR(100) NOT NULL,
+ 
+    acao VARCHAR(255) NOT NULL,
+ 
+    data_hora DATETIME NOT NULL
+)
+""")
+ 
+ 
+ 
+cursor.execute("""
+CREATE OR REPLACE VIEW vw_boletim AS
+ 
+SELECT
+ 
+    a.nome AS aluno,
+ 
+    m.nome_materia AS materia,
+ 
+    AVG(n.nota) AS media
+ 
+FROM notas n
+ 
+JOIN alunos a
+ON n.fk_id_aluno = a.id_aluno
+ 
+JOIN materias m
+ON n.fk_id_materia = m.id_materia
+ 
+GROUP BY
+a.nome,
+m.nome_materia
+""")
+ 
+# Salvar alterações
+db.commit()
+ 
+print("Banco de dados 'boletim' criado com sucesso!")
+ 
+# Fechar conexão
+cursor.close()
+db.close()
+ 
+
 cursor.close()
 db.close()
 
@@ -24,12 +158,6 @@ def conectar():
         password="Senac2026",
         database="boletim"
     )
-
-
-
-
-
-
 
 
 
--- menu.py
+++ menu.py
@@ -4,45 +4,46 @@ from arquivo_de_remocao import *
 from arquivo_cadastros import *
 from arquivo_utilitario import *
 from arquivo__de_adicionar_e_editar import *
-while True:
-     interface()
-    
-     op = input("Escolha uma opção: ")
-     validar_numero(op)
+def menu():
+     while True:
+          interface()
+     
+          op = input("Escolha uma opção: ")
+          validar_numero(op)
 
-     if op == "1":
-          cadastrar_usuario()
+          if op == "1":
+               cadastrar_usuario()
 
-     elif op == "2":
-          cadastrar_aluno()
+          elif op == "2":
+               cadastrar_aluno()
 
-     elif op == "3":
-          listar_alunos()
+          elif op == "3":
+               listar_alunos()
 
-     elif op == "4":
-          mostrar_materias()
+          elif op == "4":
+               mostrar_materias()
 
-     elif op == "5":
-          lancar_nota()
+          elif op == "5":
+               lancar_nota()
 
-     elif op == "6":
-          ver_boletim()
+          elif op == "6":
+               ver_boletim()
 
-     elif op == "7":
-          listar_notas()
+          elif op == "7":
+               listar_notas()
 
-     elif op == "8":
-          editar_nota()
+          elif op == "8":
+               editar_nota()
 
-     elif op == "9":
-          remover_nota()
+          elif op == "9":
+               remover_nota()
 
-     elif op == "10":
-          remover_usuario()
+          elif op == "10":
+               remover_usuario()
 
-     elif op == "11":
-          remover_aluno()
+          elif op == "11":
+               remover_aluno()
 
-     elif op == "0":
-          print("Saindo...")
-          break
+          elif op == "0":
+               print("Saindo...")
+               break
