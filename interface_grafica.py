ec8eae1127a81301b34ce2988bd0cf73a21cc844
Davi MB <bissondavi@gmail.com>
2026-05-27 13:21:13 -0300

    Update interface_grafica.py

--- interface_grafica.py
+++ interface_grafica.py
@@ -11,17 +11,17 @@ def interface():
                 "\n|=====================================================|\n")
 
     print("1 - |Cadastrar usuário|")
-    print("2 - |Cadastrar aluno|")
-    print("3 - |Listar alunos|")
-    print("4 - |Mostrar matérias|")
-    print("5 - |Lançar nota|")
-    print("6 - |Ver boletim|")
-    print("7 - |Listar notas|")
-    print("8 - |Editar nota|")
-    print("9 - |Remover nota|")
-    print("10- |Remover usuario|")
-    print("11- |Remover aluno|")
-    print("0 - |Sair|")
+    print("2 - |Cadastrar aluno  |")
+    print("3 - |Listar alunos    |")
+    print("4 - |Mostrar matérias |")
+    print("5 - |Lançar nota      |")
+    print("6 - |Ver boletim      |")
+    print("7 - |Listar notas     |")
+    print("8 - |Editar nota      |")
+    print("9 - |Remover nota     |")
+    print("10- |Remover usuario  |")
+    print("11- |Remover aluno    |")
+    print("0 - |Sair             |")
 
 def loading():
     for i in range(12):
