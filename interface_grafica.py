8ab0656c7e5724c4f991241a46b5c98c1af83a75
Zion Gabriel <www.ziongabrieldeoliveira2010@gmail.com>
2026-05-29 08:59:00 -0300

    Update interface_grafica.py

--- interface_grafica.py
+++ interface_grafica.py
@@ -44,7 +44,7 @@ def loading():
         time.sleep(0.2)
 
         os.system('cls' if os.name == 'nt' else 'clear')
-        ("✅ Cadastro iniciado com sucesso!")
+        ("Cadastro iniciado com sucesso!")
 
 
         
